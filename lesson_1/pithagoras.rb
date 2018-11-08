fab65c27f7d5b618c79b498c13ffbe872df6f920
mixeeff <dm.mikheev@gmail.com>
2018-11-08 23:08:06 +0300

    no nested conditions

--- lesson_1/pithagoras.rb
+++ lesson_1/pithagoras.rb
@@ -1,21 +1,22 @@
 puts "Введите длины сторон треугольника:"
 sides = []
 print "A = "
-sides[0] = gets.chomp.to_f
+sides[0] = gets.to_f
 print "B = "
-sides[1] = gets.chomp.to_f
+sides[1] = gets.to_f
 print "C = "
-sides[2] = gets.chomp.to_f
+sides[2] = gets.to_f
 sides.sort!
-if sides[0]+sides[1] < sides[2]
-	puts "Это не треугольник!"
-elsif sides[0] == sides[1] && sides[1] == sides[2]
- puts "Треугольник равносторонний"
-else
- if sides[2]**2 == sides[0]**2 + sides[1]**2
- 	puts "Треугольник прямоугольный"
- else
- 	puts "Треугольник НЕ прямоугольный"
- end
- puts "Треугольник равнобедренный" if sides[0] == sides[1]
-end 
+#определяем тип треугольника
+valid_triangle = sides[0] + sides[1] > sides[2]
+right_triangle = sides[2]**2 == sides[0]**2 + sides[1]**2
+equilateral_triangle = sides[0] == sides[1] && sides[1] == sides[2]
+isosceles_triangle = sides[0] == sides[1]
+#вывод результатов
+unless valid_triangle
+  puts "Это не треугольник!"
+  return
+end
+puts "Треугольник #{"не " unless equilateral_triangle}равносторонний" 
+puts "Треугольник #{"не " unless right_triangle}прямоугольный" 
+puts "Треугольник #{"не " unless isosceles_triangle}равнобедренный"    
