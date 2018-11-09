faf4e97fd381a8157d95f64d8c6bd10548ec1831
Dmitry Mikheev <44788086+mixeeff@users.noreply.github.com>
2018-11-09 11:34:09 +0300

    Added spaces around *

--- lesson_1/quadratic.rb
+++ lesson_1/quadratic.rb
@@ -6,7 +6,7 @@ print "b = "
 b = gets.to_f
 print "c = "
 c = gets.to_f
-if a==0
+if a == 0
   puts "Если коэффициент а=0 - это не квадратное уравнение"
   return
 end
@@ -28,12 +28,12 @@ puts equation_str
 if disсriminant < 0
   puts "не имеет корней"
 elsif disсriminant == 0
-  root = -b / (2.0*a)
+  root = -b / (2.0 * a)
   puts "имеет один корень х = #{root.round(3)}"
 else
   disсriminant_root = Math.sqrt(disсriminant)
-  root_1 = (-b + disсriminant_root) / (2.0*a)
-  root_2 = (-b - disсriminant_root) / (2.0*a)
+  root_1 = (-b + disсriminant_root) / (2.0 * a)
+  root_2 = (-b - disсriminant_root) / (2.0 * a)
   puts "имеет два корня:"
   puts "х1 = #{root_1.round(5)}"
   puts "х2 = #{root_2.round(5)}"
