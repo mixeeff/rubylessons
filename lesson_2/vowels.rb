81243fb998a7ca00097387a3c440ecb9e0e93a08
mixeeff <dm.mikheev@gmail.com>
2018-11-15 00:30:04 +0300

    Update lesson_2/vowels.rb

--- lesson_2/vowels.rb
+++ lesson_2/vowels.rb
@@ -1,5 +1,7 @@
 letters = ('A'..'Z').to_a
 vowels = %w[A E I O U]
 vowels_hash ={}
-letters.each.with_index(1) { |letter,index| vowels_hash[letter] = index if vowels.include?(letter) }
+letters.each.with_index(1) do |letter,index| 
+  vowels_hash[letter] = index if vowels.include?(letter)
+end
 puts vowels_hash
